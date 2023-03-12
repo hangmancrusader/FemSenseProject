4535866fc15cad4919871851511467f358242e93
Fatima Mustafa <fmmuree2000@gmail.com>
2023-03-12 20:33:44 +0500

    user mod commit

--- models/userpreference.js
+++ models/userpreference.js
@@ -3,6 +3,15 @@ const  mongoose = require("mongoose");
 const userpreferenceSchema = mongoose.Schema(
     {
 
+        first_name: { type: String, default: null,required: true },
+        last_name: { type: String, default: null,required: true },
+        email: { type: String, unique: true, required: true },
+        password: { type: String },
+        token: { type: String },
+        dob: { type: Date, required: true },
+        createdAt: { type: Date, default: Date.now },
+        updatedAt: { type: Date, default: Date.now },
+          
     }
 )
 
