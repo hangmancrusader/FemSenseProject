20c26fb3dbe7022c575ff9e63879c2d71dcc4d8e
Fatima Mustafa <fmmuree2000@gmail.com>
2023-03-13 00:44:43 +0500

    model commit

--- models/symptoms.js
+++ models/symptoms.js
@@ -2,9 +2,19 @@ const  mongoose = require("mongoose");
 
 const symptomsSchema = mongoose.Schema(
     {
-
-    }
-)
+        symptomId: {
+            type: Number,
+            required: true,
+            unique: true
+        },
+        description:
+        { 
+            type: [String], 
+            enum: [ "everything is fine", "cramps", "headache", "tender breasts", "acne", "cravings", "insomnia", "cravings", "bloating","constipation","nausea", "abdominal pain","fatigue","diarrhea"] 
+        } 
+       
+ }); 
+  
 
 
 module.exports =mongoose.model("Symptoms", symptomsSchema);
--- models/user.js
+++ models/user.js
@@ -2,7 +2,14 @@ const  mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema(
     {
-
+        first_name: { type: String, default: null,required: true },
+        last_name: { type: String, default: null,required: true },
+        email: { type: String, unique: true, required: true },
+        password: { type: String },
+        token: { type: String },
+        dob: { type: Date, required: true },
+        createdAt: { type: Date, default: Date.now },
+        updatedAt: { type: Date, default: Date.now },
     }
 )
 
