f066ef9a006df5ec25cc44578d52696221306f39
Dev <dev@172-15-81-71.lightspeed.frokca.sbcglobal.net>
2023-03-18 01:45:58 +0500

    Fix merge conflicts

--- app/index.js
+++ app/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 require("dotenv").config();
 const express = require('express')
 const bcrypt = require("bcryptjs");
@@ -32,5 +33,52 @@ app.use("/menstrualflow", menstraulflowRoutes);
 app.use("/insights", insightsRoutes);
 app.use("/daytracker", daytrackRoutes);
 
+=======
+index.js
+require("dotenv").config();
+const express = require('express')
+
+const jwt = require("jsonwebtoken");
+//express app that has middleware to route the endpoints to their
+const app = express()
+app.use(express.json())
+//import the routehandlers for each endpoint
+const userRoutes = require('./routes/user');
+// const symptomsRoutes = require('./routes/symptoms');
+// const periodRoutes = require('./routes/period');
+// const cycleRoutes = require('./routes/cycle');
+// const medicinereminderRoutes = require('./routes/medicinereminder');
+// const notesRoutes = require('./routes/notes');
+// const physicalActivityRoutes = require('./routes/physicalactivity');
+// const moodtrackerRoutes = require('./routes/mood');
+// const menstraulflowRoutes = require('./routes/menstrualflow');
+// const insightsRoutes = require('./routes/insights');
+// const daytrackRoutes = require('./routes/daytrack');
+
+//defining routes that handle req to given endpoints
+// app.use("/medicinereminder", medicinereminderRoutes);
+// app.use("/period", periodRoutes);
+// app.use("/cycle", cycleRoutes);
+  app.use("/user", userRoutes);
+// app.use("/symptoms", symptomsRoutes);
+// app.use("/notes", notesRoutes);
+// app.use("/physicalactivity", physicalActivityRoutes);
+// app.use("/moodtracker", moodtrackerRoutes);
+// app.use("/menstrualflow", menstraulflowRoutes);
+// app.use("/insights", insightsRoutes);
+// app.use("/daytracker", daytrackRoutes);
+const mongoose=require('mongoose')
+mongoose.connect( "mongodb+srv://notsobad_bilal:bilal786786@cluster0.r8hl8am.mongodb.net/?retryWrites=true&w=majority"
+
+);
+
+mongoose.set("strictQuery", false);
+mongoose.connection.on('error',err => {
+    console.log('Connection failed'); 
+});
+mongoose.connection.on('connected',connected=>{
+    console.log('Connected with database sucessfully'); 
+})
+>>>>>>> 6e98342 (Bilal)
  
 module.exports = app;
--- app/routes/user.js
+++ app/routes/user.js
@@ -1,6 +1,174 @@
 const express = require('express');
 const router = express.Router();
+<<<<<<< HEAD
 
 //import models
 const User = require('../../models/user')//../parentdirectory../changeparentdir
+=======
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+//import models
+const User = require('../../models/user')//../parentdirectory../changeparentdir
+router.post('/forget_password', authenticateToken,async (req, res) => {
+    const {  oldPassword, newPassword } = req.body;
+    const emm=req.user.email;
+    // Check if the user exists
+    const user = await User.findOne({ emm });
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    // Check if the old password is correct
+    const isMatch = (user.password==oldPassword);
+    if (!isMatch) {
+      return res.status(401).json({ message: 'Old password is incorrect' });
+    }
+  
+    // Generate a new password hash using the 'bcrypt' module
+ 
+  
+    // Update the user's password in the database
+    user.password = newPassword;
+    await user.save();
+  
+    res.json({ message: 'Password updated successfully' });
+  });
+  router.delete('/delete_account',authenticateToken, async (req, res) => {
+    const { email, password } = req.body;
+  
+    // Check if the user exists
+    const user = await User.findOne({ email });
+    console.log(user)
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+  
+    // Check if the password is correct
+    const isMatch = await (user.password==password)
+    if (!isMatch) {
+      return res.status(401).json({ message: 'Incorrect password' });
+    }
+    const emm = req.user.email;
+    // Delete the user account from the database
+    await User.findOneAndRemove({ email:emm }  )
+       
+  
+    res.json({ message: 'Account deleted successfully' });
+  });
+  // Get user profile endpoint
+router.get('/profile/:email',authenticateToken, async (req, res) => {
+    const { email } = req.user.email;
+  
+    // Check if the user exists
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+  console.log(user)
+    // Return the user profile information
+    res.json({
+      firstname: user.first_name,
+      lastname:user.last_name,
+      email: user.email,
+      phoneNumber: user.Phonenumber
+    });
+  });
+  // Logout endpoint
+router.post('/logout', authenticateToken,async (req, res) => {
+    const { email } = req.user.email;
+  
+    // Check if the user exists
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+  
+    // Clear the JWT token from the user document in the database
+    user.token = '';
+    await user.save();
+    res.json({ message: 'Logout successful' });
+  });
+  // Sign-up endpoint
+router.post('/signup', async (req, res) => {
+    const { email, password, first_name, last_name, Phonenumber, dob } = req.body;
+  
+    // Check if the user already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(409).json({ message: 'User already exists' });
+    }
+  
+    // Create a new user document
+    const newUser = new User({
+      email,
+      password,
+      first_name,
+      last_name,
+      Phonenumber,
+      dob
+    });
+  
+    // Hash the password before saving it to the database
+   
+  
+    // Save the new user document to the database
+    try {
+      const savedUser = await newUser.save();
+      res.json({ message: 'Sign-up successful' });
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  });
+  // Update user profile
+router.put('/:id', async (req, res) => {
+    const {  first_name, last_name, Phonenumber, dob } = req.body;
+    try {
+      const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+        
+        first_name,
+        last_name,
+        Phonenumber,
+        dob
+      }, { new: true });
+      res.json(updatedUser);
+      
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  });
+  // Middleware function to authenticate JWT token of Tutor
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if (token == null) return res.status(401).json({ message: 'JWT token is required' });
+  
+    jwt.verify(token, 'secret', (err, user) => {
+      if (err) return res.status(403).json({ message: 'Invalid JWT token' });
+      req.user = user;
+      next();
+    });
+  }
+  // Login API
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+  
+    // Check if the user exists
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: 'Invalid email or password' });
+    }
+  
+    // Check if the password is correct
+    const validPassword = (password== user.password);
+    if (!validPassword) {
+      return res.status(400).json({ message: 'Invalid email or password' });
+    }
+  
+    // Generate a JWT token
+    const token = jwt.sign({ id: user._id, email: user.email }, 'secret', { expiresIn: '1h' });
+  
+    res.json({ token });
+  });
+  
+>>>>>>> 6e98342 (Bilal)
 module.exports = router;
